d92c4b2ad172250c4357e3f6f8f8149f90c97f1a
arielLevita <levita.ariel@gmail.com>
2023-02-06 00:42:48 -0300

    Borrador de sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { PreguntasComponent } from './components/preguntas/preguntas.component';
 import { DetallePreguntaComponent } from './components/detalle-pregunta/detalle-pregunta.component';
 import { MessagesComponent } from './components/messages/messages.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PreguntasComponent,
     DetallePreguntaComponent,
-    MessagesComponent
+    MessagesComponent,
+    SidebarComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
